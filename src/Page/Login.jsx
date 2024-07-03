9ebe042ef818317a06a6836e44b06ba659d7f510
Sasith Methmal <52422394+sasithmj@users.noreply.github.com>
2024-07-03 17:13:49 +0530

    Authentication and login with hash password

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,181 +1,225 @@
-import React, { useState } from 'react';
-import './login.css';
+import React, { useState } from "react";
+import "./login.css";
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-//  import Validation from "./LoginValidation";
-//  import ValidationSignup from './SignupValidation';
- import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie"; // Import js-cookie
 
 export default function Login() {
+  const [action, setAction] = useState("");
+  const [rememberMe, setRememberMe] = useState(false); // State for remember me
+  const [values, setValues] = useState({
+    name: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    contactNumber: "",
+    address: "",
+    email: "",
+  });
+  const [loginvalues, setLoginValues] = useState({
+    email: "",
+    address: "",
+  });
 
-const [action, setAction] = useState('');
-const registerLink = () => {
-    setAction(' active')
-};
-const loginLink = () => {
-    setAction('')
-   
+  const registerLink = () => {
+    setAction(" active");
+  };
+  const loginLink = () => {
+    setAction("");
+  };
+  const navigate = useNavigate();
 
-};
-
-
-const  [values, setValues] = useState({
-    name: '',
-    username: '',
-    password: '',
-    confirmPassword: '',
-    contactNumber: '',
-    address: '',
-    email: '',
-    
-})
-const navigate = useNavigate();
-// const [errors, setErrors] = useState({
-
-// });
-
-const handleInput = (event) => {
+  const handleInput = (event) => {
     const { name, value } = event.target;
     setValues({ ...values, [name]: value });
-};
+  };
+  const handleLoginInput = (event) => {
+    const { name, value } = event.target;
+    setLoginValues({ ...loginvalues, [name]: value });
+  };
 
-const handleSubmitSignup = (event) => {
+  const handleRememberMe = (event) => {
+    setRememberMe(event.target.checked);
+  };
+
+  const handleSubmitSignup = (event) => {
     event.preventDefault();
 
     if (values.password !== values.confirmPassword) {
-        alert("Passwords do not match");
-        return;
+      alert("Passwords do not match");
+      return;
     }
-    axios.post('http://localhost:8088/register', values)
-    .then(res => 
-        //  console.log("register successfuly")
-         navigate('/home'))
-         .catch(err => console.log(err));
 
-    // setErrors(ValidationSignup(values));
-    // if(errors.name === "" && errors.username === "" && errors.password === "" && errors.contactNumber === "" 
-    //     && errors.address === "" && errors.email === ""){
-    //         axios.get('http://localhost:8085/registercustomer', values)
-    //         .then(res => {
-    //             // console.log('server response',res.data);
-    //             navigate('/shop');
-    //         })
-    //         .catch(err => console.log(err));
-    // }
-};
+    axios
+      .post("http://localhost:8088/register", values)
+      .then((res) => navigate("/home"))
+      .catch((err) => console.log(err));
+  };
 
-const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('http://localhost:8088/login', values)
-    .then(res => {
-        if(res.data === "success"){
-            navigate('/home');
+    axios
+      .post("http://localhost:8088/login", loginvalues)
+      .then((res) => {
+        console.log(loginvalues);
+        if (res.data.status === "success") {
+          const token = res.data.token;
 
+          if (rememberMe) {
+            Cookies.set("token", token, { expires: 7 }); // Save token for 7 days
+          } else {
+            Cookies.set("token", token); // Session cookie
+          }
+
+          navigate("/home");
+        } else {
+          alert("Incorrect username or password!");
         }
-        else{
-            alert("incorrect username or password!!!!");
-        }})
-         .catch(err => console.log(err));
-};
-
-
-
-
-
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="form-box-body">
-    <div className={`wrapper${action}`}>
+      <div className={`wrapper${action}`}>
         <div className="form-box login">
-            <form onSubmit={handleSubmit}>    
-                <h1>Login</h1>
-                <div className="input-box">
-                    <input type="text" placeholder='Username' required  onChange={handleInput} name='username'/>
-                    {/* {errors.username && <span className='text-danger'>{errors.username}</span>} */}
-                    <FaUser  className='icon'/>
-
-                </div>
-                <div className="input-box">
-                    <input type="password" placeholder='Password' required onChange={handleInput} name='password'/>
-                    {/* {errors.password && <span className='text-danger'>{errors.password}</span>} */}
-                    <FaLock  className='icon'/>
-
-                </div>
-                <div className="remember-forgot">
-                    <label><input type="checkbox" />Remember me</label>
-                    <a href="#">Forgot password?</a>
-                </div>
-
-                <button type="submit" name='ubmit'>Login</button>
-                <div className="register-link">
-                    <p>Dont have an account? <a href="#" onClick={registerLink}>Register</a></p>
-                </div>
-            </form>
+          <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Username"
+                required
+                onChange={handleLoginInput}
+                name="email"
+              />
+              <FaUser className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                onChange={handleLoginInput}
+                name="password"
+              />
+              <FaLock className="icon" />
+            </div>
+            <div className="remember-forgot">
+              <label>
+                <input type="checkbox" onChange={handleRememberMe} />
+                Remember me
+              </label>
+              <a href="#">Forgot password?</a>
+            </div>
+            <button type="submit" name="submit">
+              Login
+            </button>
+            <div className="register-link">
+              <p>
+                Don't have an account?{" "}
+                <a href="#" onClick={registerLink}>
+                  Register
+                </a>
+              </p>
+            </div>
+          </form>
         </div>
         <div className="form-box register">
-            <form onSubmit={handleSubmitSignup}>
-                <h1>Registration</h1>
-                <div className="input-box">
-                    <input type="text" placeholder='Name' required name='name' onChange={handleInput}/>
-                    {/* {errors.user && <span className='text-danger'>{errors.user}</span>} */}
-                    <FaUser  className='icon'/>
-
-                </div>
-                <div className="input-box">
-                    <input type="email" placeholder='Email' required name='email' onChange={handleInput}/>
-                    {/* {errors.email && <span className='text-danger'>{errors.email}</span>} */}
-                    <MdEmail  className='icon'/>
-
-                </div>
-                <div className="input-box">
-                    <input type="text" placeholder='Contact Number' required name='contactNumber' onChange={handleInput}/>
-                    {/* {errors.contactNumber && <span className='text-danger'>{errors.contactNumber}</span>} */}
-                    <MdEmail  className='icon'/>
-
-                </div>
-                <div className="input-box">
-                    <input type="text" placeholder='Address' required name='address' onChange={handleInput}/>
-                    {/* {errors.address && <span className='text-danger'>{errors.address}</span>} */}
-                    <MdEmail  className='icon'/>
-
-                </div>
-                <div className="input-box">
-                    <input type="text" placeholder='Username' required name='username'onChange={handleInput}/>
-                    {/* {errors.username && <span className='text-danger'>{errors.username}</span>} */}
-                    <FaLock  className='icon'/>
-
-                </div>
-                <div className="input-box">
-                    <input type="password" placeholder='Password' required name='password' onChange={handleInput}/>
-                    {/* {errors.password && <span className='text-danger'>{errors.password}</span>} */}
-                    <FaLock  className='icon'/>
-
-                </div>
-                <div className="input-box">
-                    <input type="password" placeholder='Confirm Password' required name='confirmPassword' onChange={handleInput}/>
-                    {/* {errors.password && <span className='text-danger'>{errors.password}</span>} */}
-                    <FaLock  className='icon'/>
-
-                </div>
-                <div className="remember-forgot">
-                    <label><input type="checkbox" />I agree to the terms & conditions</label>
-                    
-                </div>
-
-                <button type="submit" name='submit'>Register</button>
-                <div className="register-link">
-                    <p>Already have an account? <a href="#" onClick={loginLink}>Login</a></p>
-                </div>
-            </form>
+          <form onSubmit={handleSubmitSignup}>
+            <h1>Registration</h1>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Name"
+                required
+                name="name"
+                onChange={handleInput}
+              />
+              <FaUser className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                name="email"
+                onChange={handleInput}
+              />
+              <MdEmail className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Contact Number"
+                required
+                name="contactNumber"
+                onChange={handleInput}
+              />
+              <MdEmail className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Address"
+                required
+                name="address"
+                onChange={handleInput}
+              />
+              <MdEmail className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Username"
+                required
+                name="username"
+                onChange={handleInput}
+              />
+              <FaLock className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                name="password"
+                onChange={handleInput}
+              />
+              <FaLock className="icon" />
+            </div>
+            <div className="input-box">
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                required
+                name="confirmPassword"
+                onChange={handleInput}
+              />
+              <FaLock className="icon" />
+            </div>
+            <div className="remember-forgot">
+              <label>
+                <input type="checkbox" />I agree to the terms & conditions
+              </label>
+            </div>
+            <button type="submit" name="submit">
+              Register
+            </button>
+            <div className="register-link">
+              <p>
+                Already have an account?{" "}
+                <a href="#" onClick={loginLink}>
+                  Login
+                </a>
+              </p>
+            </div>
+          </form>
         </div>
-
-
-
+      </div>
     </div>
-    </div>
-  )
+  );
 }
